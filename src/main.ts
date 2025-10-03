b9cf6ddfdd7af1225c4ff4024e97af420e10de4e
DevinL04 <123677317+DevinL04@users.noreply.github.com>
2025-10-03 02:19:47 +0000

    newest changes

--- src/main.ts
+++ src/main.ts
@@ -23,5 +23,6 @@ const counterElement = document.getElementById("counter")!;
 button.addEventListener("click", () => {
   // This looks like to a good place to add some logic!
   counter += 67;
+  counterElement.textContent = counter.toString();
   console.log("I have these thingies:", button, counterElement, counter);
 });
